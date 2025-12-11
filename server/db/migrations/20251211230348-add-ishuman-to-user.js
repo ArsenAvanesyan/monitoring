2c557b2b58f61d34bd9c07aedb712e84997e55fa
Max Kosheutov <maxkosh1994@gmail.com>
2025-12-11 23:06:17 +0300

    test: add isHuman column to Users model

---
+++ server/db/migrations/20251211230348-add-ishuman-to-user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'isHuman', {
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
+      defaultValue: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'isHuman');
+  },
+};
+
